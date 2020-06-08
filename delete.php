f91e187fb1cec9baa5c1164a96be145e8b6d0a28
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-08 13:02:52 +0300

    Update delete.php

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+    require_once './db.php';
+
+    $id = $_GET['id'];
+
+    $sql = 'DELETE FROM `tasks` WHERE `id` = ?';
+    $query = $pdo->prepare($sql);
+    $query->execute([$id]);
+
+    header('Location: /');
